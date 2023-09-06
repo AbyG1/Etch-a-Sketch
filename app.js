22b79e157eff5a06863b36e0fc68b37bb67586c2
Aby George <abygeorge133@gmail.com>
2023-09-07 00:30:12 +0530

    Made grid using js

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
-const containerDiv = document.querySelector('#container')
+const containerDiv = document.querySelector('#container');
 
-for(let i = 0; i < 16; i++) {
-    const gridDiv = document.createElement('div');
-    gridDiv.classList.add('grid');
-    containerDiv.appendChild(gridDiv);
-    
-    gridDiv.addEventListener('mouseover',() => {
-        gridDiv.classList.add('black');
-    })
+
+
+const sizeButton = document.querySelector('#grid-size');
+
+createDiv(16);
+
+
+
+
+sizeButton.addEventListener('click',() => {
+    let size;
+    do {
+       size = prompt("Enter a size between 0 and 100"); 
+    } while (size < 1 || size > 100)
+     
+    console.log(size);
+    createDiv(size);
+})
+ 
+
+
+function createDiv(size) {
+    for(let i = 0; i < size; i++) {
+        for(let j = 0; j < size; j++) {
+            const gridDiv = document.createElement('div');
+            gridDiv.style.width = `${800/size}px`;
+            gridDiv.style.height = `${800/size}px`;
+            containerDiv.appendChild(gridDiv);
+            gridDiv.addEventListener('mouseover',() => {
+            gridDiv.style.setProperty('background-color','violet');
+        })
+        
+    }
     
 }
+}
+
+
+
 
 
 
