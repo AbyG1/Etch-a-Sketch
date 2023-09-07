598ab75ca24f4da69b10206ffa9e1f6066375777
Aby George <abygeorge133@gmail.com>
2023-09-07 15:30:33 +0530

    Implement reset grid functionality

--- app.js
+++ app.js
@@ -1,41 +1,64 @@
 const containerDiv = document.querySelector('#container');
 
-
+let divArray = [];
 
 const sizeButton = document.querySelector('#grid-size');
 
-createDiv(16);
+modifyDiv(16);
 
 
 
 
 sizeButton.addEventListener('click',() => {
+    
     let size;
     do {
        size = prompt("Enter a size between 0 and 100"); 
     } while (size < 1 || size > 100)
      
-    console.log(size);
-    createDiv(size);
+    removeDiv();
+    modifyDiv(size);
 })
  
 
 
-function createDiv(size) {
-    for(let i = 0; i < size; i++) {
-        for(let j = 0; j < size; j++) {
+function createDiv(size) { 
             const gridDiv = document.createElement('div');
             gridDiv.style.width = `${800/size}px`;
             gridDiv.style.height = `${800/size}px`;
-            containerDiv.appendChild(gridDiv);
-            gridDiv.addEventListener('mouseover',() => {
-            gridDiv.style.setProperty('background-color','violet');
-        })
+            return gridDiv
+           
         
     }
     
+
+
+
+
+function modifyDiv(size) {
+    const noOfGrid = size * size;
+    for(let i = 0; i < noOfGrid; i++) {
+        
+            divArray.push(createDiv(size));
+            containerDiv.appendChild(divArray[i]);
+            divArray[i].addEventListener('mouseover',() => {
+            divArray[i].style.setProperty('background-color','black');
+            
+        })
+    }
+   
 }
-}
+
+    function removeDiv() {
+        while (containerDiv.firstChild) {
+            containerDiv.removeChild(containerDiv.firstChild);
+        }
+        
+        divArray = [];
+    }
+    
+    
+
 
 
 
